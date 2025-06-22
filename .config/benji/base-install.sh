48be16738db539b4c9090ebed200a234d6afcba9
Adam Nachtrab <58910398+abnachtrab@users.noreply.github.com>
2025-06-21 23:20:14 -0700

    Update base-install.sh

--- .config/benji/base-install.sh
+++ .config/benji/base-install.sh
@@ -6,35 +6,21 @@ sudo pacman -Sy
 # System Stuff
 sudo pacman -S --noconfirm              \
 linux linux-firmware linux-headers      \
-base base-devel                         \
+base base-devel                        \
 efibootmgr                              \
 btrfs-progs zram-generator
 
 # Processor and GPU Things
 sudo pacman -S --noconfirm              \
 intel-ucode sof-firmware                \
-xf86-video-intel dkms nvidia-dkms       \
-libva-nvidia-driver nvidia-prime        \
-nvidia-settings
-
-# Networking
-sudo pacman -S --noconfirm bluez        \
-bluez-utils bluetui networkmanager      \
-iwd network-manager-applet              \
-wireless_tools
-
-# Audio
-sudo pacman -S --noconfirm              \
-pipewire                                \
-gst-plugin-pipewire                     \
-pipewire-alsa pipewire-jack             \
-pipewire-pulse wireplumber
+xf86-video-intel dkms nvidia-dkms      \
+libva-nvidia-driver nvidia-settings    \
 
 # CLI Stuff
 sudo pacman -S --noconfirm              \
-brightnessctl fastfetch git htop jq     \
-man-db nano playerctl rclone            \
-smartmontools unzip wget zip zsh
+fastfetch git htop                     \
+man-db nano rclone                     \
+unzip wget zip zsh
 
 # Install yay
 if ! command -v yay &>/dev/null; then
@@ -46,37 +32,9 @@ if ! command -v yay &>/dev/null; then
     yay -Sy
 fi
 
-# Hyprland Backend
-sudo pacman -S --noconfirm              \
-greetd hyprland uwsm                    \
-qt5-wayland qt6-wayland                 \
-xdg-desktop-portal-hyprland             \
-xorg-xinit
-yay -S --no-confirm howdy-git
-
-# Autologin
-sudo tee /etc/greetd/config.toml > /dev/null << 'EOF'
-[terminal]
-vt = 1
-[default_session]
-command = "sh /etc/greetd/bootmode"
-user = "benji"
-EOF
-
 # Desktop Stuff
-sudo pacman -S --noconfirm              \
-dunst grim hyprbar hyprlock hyprpaper   \
-kitty pavucontrol rofi-wayland slurp
+sudo pacman -S --noconfirm kitty plasma
 yay -S --noconfirm brave-bin
-
-# Install eww
-git clone https://github.com/elkowar/eww
-cd eww
-cargo build --release --no-default-features --features=wayland
-chmod +x target/release/eww
-sudo mv target/release/eww /usr/bin/
-cd ..
-rm -rf eww
 
 # Set default shell
 sudo chsh -s $(which zsh) benji >/dev/null 2>&1
@@ -86,6 +44,10 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     export RUNZSH=no CHSH=no KEEP_ZSHRC=yes && \
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Update everything
+sudo pacman -Syu --noconfirm
+sudo yay -Syu --noconfirm
 
 # Cleanup
 rm -rf ~/.bash*
@@ -99,6 +61,5 @@ if [[ "$answer" == "y" ]]; then
 fi
 
 # Things that may be wanted, but do manually:
-#  - razer-laptop-control-no-dkms
 #  - howdy
 #  - xpadneo-dkms (for Xbox Elite Series 2 Support)
